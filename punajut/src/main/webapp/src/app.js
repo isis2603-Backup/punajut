d1621e307939f462dcf2b0a339c2bae9aece2678
rangel10 <>
2016-01-28 06:08:37 -0500

    Creación del proyecto web.

---
+++ punajut/src/main/webapp/src/app.js
@@ -0,0 +1,17 @@
+(function (ng) {
+
+    var mod = ng.module("mainApp", ["ui.router"]);
+
+    mod.config(['$logProvider', function ($logProvider) {
+            $logProvider.debugEnabled(true);
+        }]);
+
+    mod.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+            $urlRouterProvider.otherwise("/evento");
+            $stateProvider
+                    .state('evento', {
+                        url: '/evento',
+                        templateUrl: "src/modules/book/book.tpl.html"
+                    });
+        }]);
+})(window.angular);
