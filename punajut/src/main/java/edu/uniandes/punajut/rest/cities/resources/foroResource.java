7aff6ce492c1d93f65c904797807e70bdf87ce42
rangel10 <ra.angel10@172.24.99.232>
2016-03-02 06:11:39 -0500

    java class added

--- punajut/src/main/java/edu/uniandes/punajut/rest/cities/resources/foroResource.java
+++ punajut/src/main/java/edu/uniandes/punajut/rest/cities/resources/foroResource.java
@@ -4,13 +4,23 @@
  * and open the template in the editor.
  */
 package edu.uniandes.punajut.rest.cities.resources;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PUT;
+
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 
 /**
  *
  * @author ra.angel10
  */
 @Path("cities")
-@Produces
+@Produces("application/json")
 public class foroResource {
 
 }
