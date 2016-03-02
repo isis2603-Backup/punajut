bd79cefd62ae7e6e15d09c7d99aa8cfa29067b01
lshernandez10 <ls.hernandez10@uniandes.edu.co>
2016-03-02 11:08:45 -0500

    evento visualizacion resource

---
+++ punajut/src/main/java/edu/uniandes/punajut/rest/cities/resources/eventoVisualizacionResource.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.uniandes.punajut.rest.cities.resources;
+
+import javax.ws.rs.Path;
+
+/**
+ *
+ * @author ls.hernandez10
+ */
+@Path("../../Web Pages/src/modules/eventoVisual")
+public class eventoVisualizacionResource {
+
+}
