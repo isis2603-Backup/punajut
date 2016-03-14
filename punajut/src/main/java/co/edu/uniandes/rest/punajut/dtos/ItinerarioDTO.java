c0c3cd0c452da16034933a7c70c6e427840dbea1
miarevalo10 <mi.arevalo10@uniandes.edu.co>
2016-03-14 12:43:54 -0500

    ItinerarioDTO

--- punajut/src/main/java/co/edu/uniandes/rest/punajut/dtos/ItinerarioDTO.java
+++ punajut/src/main/java/co/edu/uniandes/rest/punajut/dtos/ItinerarioDTO.java
@@ -5,10 +5,101 @@
  */
 package co.edu.uniandes.rest.punajut.dtos;
 
+import java.sql.Date;
+
 /**
  *
  * @author mi.arevalo10
  */
-public class ItinerarioDTO {
+public class ItinerarioDTO
+{
+    private Long id;
+    private String name;
+    private Date fechaInicio;
+    private Date fechaFin;
+//    private ArrayList<CiudadDTO> ciudades;
 
+
+    /**
+     * Constructor por defecto
+     */
+    public ItinerarioDTO()
+    {
+    }
+
+    /**
+     * Constructor con parámetros.
+     * @param id identificador del itinerario
+     * @param name nombre del itinerario
+     */
+    public ItinerarioDTO(Long id, String name, Date fechaI, Date fechaF) {
+		super();
+		this.id = id;
+		this.name = name;
+                fechaInicio = fechaI;
+                fechaFin = fechaF;
+	}
+
+	/**
+     * @return the id
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the initial date
+     */
+    public Date getFechaInicio() {
+        return fechaInicio;
+    }
+
+    /**
+     * @param pFechaInicio fecha to set
+     */
+    public void setFechaInicio(Date pFechaInicio) {
+        this.fechaInicio = pFechaInicio;
+    }
+   /**
+     * @return the final date
+     */
+    public Date getFechaFin() {
+        return fechaFin;
+    }
+
+    /**
+     * @param pFechaFin date to set
+     */
+    public void setFechaFin(Date pFechaFin) {
+        this.fechaFin = pFechaFin;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Convierte el objeto a una cadena
+     */
+    @Override
+    public String toString() {
+    	return "{ id : " + getId() + ", name : \"" + getName() + "\" }" ;
+    }
 }
