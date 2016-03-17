f9f0c8b5f410c4eb91640869946f768ec0f5cecc
rangel10 <ra.angel10@172.24.99.232>
2016-03-17 02:49:25 -0500

    Actualizado rest de foro

--- punajut/src/main/java/co/edu/uniandes/rest/punajut/mocks/ForoLogicMock.java
+++ punajut/src/main/java/co/edu/uniandes/rest/punajut/mocks/ForoLogicMock.java
@@ -9,17 +9,67 @@ import co.edu.uniandes.rest.punajut.dtos.CategoriaDTO;
 import co.edu.uniandes.rest.punajut.dtos.DiscusionDTO;
 import co.edu.uniandes.rest.punajut.dtos.ForoDTO;
 import co.edu.uniandes.rest.punajut.dtos.RespuestaDTO;
+import co.edu.uniandes.rest.punajut.dtos.UsuarioDTO;
 import co.edu.uniandes.rest.punajut.exceptions.ForoLogicException;
+import java.util.ArrayList;
 import java.util.List;
+import javax.inject.Named;
+import javax.inject.Singleton;
 
 /**
  *
  * @author ra.angel10
  */
+@Named
+@Singleton
 public class ForoLogicMock {
 
     private ForoDTO foro;
 
+
+    public ForoLogicMock()  {
+        RespuestaDTO init1 = new RespuestaDTO(2, "testDiscusion", new UsuarioDTO(3,"testUser"));
+        RespuestaDTO init2 = new RespuestaDTO(4, "testDiscusion", new UsuarioDTO(99,"testUser"));
+        RespuestaDTO init3 = new RespuestaDTO(10, "testDiscusion", new UsuarioDTO(32,"testUser"));
+
+        DiscusionDTO d1 = new DiscusionDTO(1, "test1", new UsuarioDTO(100, "test100"), init1, DiscusionDTO.OPEN);
+        DiscusionDTO d2 = new DiscusionDTO(55, "test2", new UsuarioDTO(101, "test101"), init2, DiscusionDTO.OPEN);
+        DiscusionDTO d3 = new DiscusionDTO(98, "test3", new UsuarioDTO(102, "test102"), init3, DiscusionDTO.OPEN);
+        RespuestaDTO r1 = new RespuestaDTO(45, "TestReplyPost", new UsuarioDTO(23, "test23"));
+        RespuestaDTO r2 = new RespuestaDTO(46, "TestReplyPost", new UsuarioDTO(24, "test24"));
+        RespuestaDTO r3 = new RespuestaDTO(47, "TestReplyPost", new UsuarioDTO(25, "test25"));
+        d1.addRespuesta(r1);
+        d1.addRespuesta(r2);
+        d1.addRespuesta(r3);
+
+        d2.addRespuesta(r1);
+        d2.addRespuesta(r2);
+        d2.addRespuesta(r3);
+
+        d3.addRespuesta(r1);
+        d3.addRespuesta(r2);
+        d3.addRespuesta(r3);
+
+        CategoriaDTO cat1 = new CategoriaDTO(209, "Categoria1");
+        CategoriaDTO cat2 = new CategoriaDTO(219, "Categoria2");
+        CategoriaDTO cat3 = new CategoriaDTO(229, "Categoria3");
+        cat1.addDiscusion(d1);
+        cat1.addDiscusion(d2);
+        cat1.addDiscusion(d3);
+
+        cat2.addDiscusion(d1);
+        cat2.addDiscusion(d2);
+        cat2.addDiscusion(d3);
+
+        cat3.addDiscusion(d1);
+        cat3.addDiscusion(d2);
+        cat3.addDiscusion(d3);
+        ArrayList<CategoriaDTO> a = new ArrayList<>();
+        a.add(cat1);
+        a.add(cat2);
+        a.add(cat3);
+        foro = new ForoDTO(1000, a);
+    }
     public List<String> getCategorias() throws ForoLogicException{
         return foro.getCategoriasName();
     }
