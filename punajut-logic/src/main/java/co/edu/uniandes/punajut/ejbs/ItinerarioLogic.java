373fbb648e550854f66a93a5da989e9e6c6e516e
miarevalo10 <mi.arevalo10@uniandes.edu.co>
2016-03-30 09:33:22 -0500

    Agregado persistence.xml y clases de logica itinerario

---
+++ punajut-logic/src/main/java/co/edu/uniandes/punajut/ejbs/ItinerarioLogic.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.punajut.ejbs;
+
+import co.edu.uniandes.punajut.api.IItinerarioLogic;
+import co.edu.uniandes.punajut.entities.ItinerarioEntity;
+import co.edu.uniandes.csw.bookstore.persistence.ItinerarioPersistence;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+
+/**
+ *
+ * @author mi.arevalo10
+ */
+public class ItinerarioLogic
+{
+
+}
