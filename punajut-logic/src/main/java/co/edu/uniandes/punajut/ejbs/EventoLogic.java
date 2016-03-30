017a2ad73bdce95ef789fe0018ed4f1f527fc71f
rcardenas11 <r.cardenas11@uniandes.edu.co>
2016-03-30 11:26:21 -0500

    Se agrego EventoLogic, EventoEntity y EventoPersitence

---
+++ punajut-logic/src/main/java/co/edu/uniandes/punajut/ejbs/EventoLogic.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.punajut.ejbs;
+
+import co.edu.uniandes.punajut.entities.EventoEntity;
+import java.util.List;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author r.cardenas11
+ */
+@Stateless
+public class EventoLogic {
+    private static final Logger logger = Logger.getLogger(AuthorLogic.class.getName());
+
+
+
+
+    @Inject
+    private EventoPersistence eventoPersistence;
+
+    @Override
+    public List<EventoEntity> getAuthors() {
+        logger.info("Inicia proceso de consultar todos los autores");
+        List<EventoEntity> authors = eventoPersistence.findAll();
+        logger.info("Termina proceso de consultar todos los autores");
+        return authors;
+    }
+
+}
