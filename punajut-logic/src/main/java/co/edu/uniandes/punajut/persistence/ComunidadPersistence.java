1fe7673d3f2e21a16aaadafb27f220a1c844cbb2
ja.poveda10 <ja.poveda10@172.24.100.225>
2016-03-30 12:00:19 -0500

    Se agregó IComunidadLogic, ComunidadLogic, ComunidadEntity y ComunidadPersistance a punajut-logic

---
+++ punajut-logic/src/main/java/co/edu/uniandes/punajut/persistence/ComunidadPersistence.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.punajut.persistence;
+
+import co.edu.uniandes.csw.bookstore.entities.AuthorEntity;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+/**
+ *
+ * @author ja.poveda10
+ */
+@Stateless
+public class ComunidadPersistence {
+
+    private static final Logger logger = Logger.getLogger(ComunidadPersistence.class.getName());
+
+    @PersistenceContext(unitName = "PunajutPU")
+    protected EntityManager em;
+
+    public List<ItinerarioEntity> findAll() {
+        logger.info("Consultando todos los itinerarios de comunidad");
+        Query q = em.createQuery("select u from ComunidadEntity u");
+        return q.getResultList();
+    }
+
+}
