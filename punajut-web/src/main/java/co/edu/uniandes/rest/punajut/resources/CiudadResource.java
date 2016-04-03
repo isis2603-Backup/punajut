b67cef539d3431dd387330bde0c9bd396f97749a
ja.poveda10 <ja.poveda10@172.24.100.225>
2016-04-03 11:46:08 -0500

    Correciones en pom.xml, Ciudad Resource y Ciudad Converter

--- punajut-web/src/main/java/co/edu/uniandes/rest/punajut/resources/CiudadResource.java
+++ punajut-web/src/main/java/co/edu/uniandes/rest/punajut/resources/CiudadResource.java
@@ -5,12 +5,14 @@
  */
 package co.edu.uniandes.rest.punajut.resources;
 
+import co.edu.uniandes.punajut.api.ICiudadLogic;
 import co.edu.uniandes.rest.punajut.dtos.CiudadDTO;
 import co.edu.uniandes.rest.punajut.exceptions.CiudadLogicException;
 import co.edu.uniandes.rest.punajut.mocks.CiudadLogicMock;
 import java.util.List;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -18,33 +20,38 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 /**
- * Clase que implementa el recurso REST correspondiente a "ciudades".
- * Al ejecutar la aplicación, el recurso será accesibe a través de la
- * ruta "/api/ciudades"
+ * Clase que implementa el recurso REST correspondiente a "ciudades". Al
+ * ejecutar la aplicación, el recurso será accesibe a través de la ruta
+ * "/api/ciudades"
+ *
  * @author ja.poveda10
  */
 @Path("ciudades")
-@Produces("application/json")
-@RequestScoped
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 
 public class CiudadResource {
-    @Inject
-	CiudadLogicMock ciudadLogic;
 
-	/**
-	 * Obtiene el listado de ciudades.
-	 * @return lista de ciudades
-	 * @throws CiudadLogicException excepción retornada por la lógica
-	 */
+    @Inject
+    ICiudadLogic ciudadLogic;
+
+    /**
+     * Obtiene la lista de los recursos
+     *
+     * @return lista de ciudades
+     * @generated
+     */
     @GET
-    public List<CiudadDTO> getCiudades() throws CiudadLogicException {
-        return ciudadLogic.getCiudades();
+    public List<CiudadDTO> getCiudades() {
+        return CiudadConverter.listEntity2DTO(ciudadLogic.getCiudades());
     }
 
     /**
      * Obtiene una ciudad
+     *
      * @param id identificador de la ciudad
      * @return ciudad encontrada
      * @throws CiudadLogicException cuando la ciudad no existe
@@ -57,9 +64,11 @@ public class CiudadResource {
 
     /**
      * Agrega una ciudad
+     *
      * @param ciudad ciudad a agregar
      * @return datos de la ciudad a agregar
-     * @throws CiudadLogicException cuando ya existe una ciudad con el id suministrado
+     * @throws CiudadLogicException cuando ya existe una ciudad con el id
+     * suministrado
      */
     @POST
     public CiudadDTO createCiudad(CiudadDTO ciudad) throws CiudadLogicException {
@@ -68,10 +77,12 @@ public class CiudadResource {
 
     /**
      * Actualiza los datos de una ciudad
+     *
      * @param id identificador de la ciudad a modificar
      * @param ciudad ciudad a modificar
      * @return datos de la ciudad modificada
-     * @throws CiudadLogicException cuando no existe una ciudad con el id suministrado
+     * @throws CiudadLogicException cuando no existe una ciudad con el id
+     * suministrado
      */
     @PUT
     @Path("{id: \\d+}")
@@ -81,12 +92,14 @@ public class CiudadResource {
 
     /**
      * Elimina los datos de una ciudad
+     *
      * @param id identificador de la ciudad a eliminar
-     * @throws CiudadLogicException cuando no existe una ciudad con el id suministrado
+     * @throws CiudadLogicException cuando no existe una ciudad con el id
+     * suministrado
      */
     @DELETE
     @Path("{id: \\d+}")
     public void deleteCiudad(@PathParam("id") Long id) throws CiudadLogicException {
-    	ciudadLogic.deleteCiudad(id);
+        ciudadLogic.deleteCiudad(id);
     }
 }
