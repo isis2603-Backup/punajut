af6b94b6a88fce81c39fff676272570db79cd0b8
lshernandez10 <ls.hernandez10@uniandes.edu.co>
2016-04-04 19:28:20 -0500

    Evento viajero

---
+++ punajut-logic/src/main/java/co/edu/uniandes/punajut/api/IEventoViajeroLogic.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.punajut.api;
+import co.edu.uniandes.punajut.api.IEventoViajeroLogic;
+
+/**
+ * @author ls.hernandez10
+ */
+public interface IEventoViajeroLogic IEventoViajeroLogic
+{
+
+}
