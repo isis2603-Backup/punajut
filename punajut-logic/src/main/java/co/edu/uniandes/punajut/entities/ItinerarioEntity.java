0a8957626df76caeb18f7c576a79c075ae7f501d
jpgonzalez14 <jp.gonzalez14@uniandes.edu.co>
2016-04-13 05:59:34 -0500

    viajero entity update

--- punajut-logic/src/main/java/co/edu/uniandes/punajut/entities/ItinerarioEntity.java
+++ punajut-logic/src/main/java/co/edu/uniandes/punajut/entities/ItinerarioEntity.java
@@ -9,6 +9,8 @@ import javax.persistence.Entity;
 import co.edu.uniandes.csw.crud.spi.entity.BaseEntity;
 import java.io.Serializable;
 import java.util.Date;
+import javax.persistence.CascadeType;
+import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
@@ -19,6 +21,7 @@ import javax.persistence.TemporalType;
  */
 public class ItinerarioEntity extends BaseEntity implements Serializable
 {
+
     @Temporal(TemporalType.DATE)
     private Date fechaInicio;
     @Temporal(TemporalType.DATE)
