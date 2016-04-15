c7355ddb38cead9064158b012713a3d349c36d15
ja.poveda10 <ja.poveda10@172.24.100.225>
2016-04-15 10:11:51 -0500

    Se agregaron metodos de prueba en CiudadPersistenceTest

--- punajut-logic/src/test/java/co/edu/uniandes/punajut/persistence/CiudadPersistenceTest.java
+++ punajut-logic/src/test/java/co/edu/uniandes/punajut/persistence/CiudadPersistenceTest.java
@@ -6,6 +6,8 @@
 package co.edu.uniandes.punajut.persistence;
 
 import co.edu.uniandes.punajut.entities.CiudadEntity;
+import java.util.ArrayList;
+import java.util.List;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -17,6 +19,7 @@ import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.Assert;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.Before;
 import org.junit.runner.RunWith;
 import uk.co.jemos.podam.api.PodamFactory;
 import uk.co.jemos.podam.api.PodamFactoryImpl;
@@ -34,6 +37,9 @@ public class CiudadPersistenceTest {
     @PersistenceContext
     private EntityManager em;
 
+    @Inject
+    UserTransaction utx;
+
     private final PodamFactory factory = new PodamFactoryImpl();
 
     public CiudadPersistenceTest() {
@@ -48,6 +54,38 @@ public class CiudadPersistenceTest {
                 .addAsManifestResource("META-INF/beans.xml", "beans.xml");
     }
 
+    @Before
+    public void configTest() {
+        try {
+            utx.begin();
+            em.joinTransaction();
+            clearData();
+            insertData();
+            utx.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            try {
+                utx.rollback();
+            } catch (Exception e1) {
+                e1.printStackTrace();
+            }
+        }
+    }
+
+    private void clearData() {
+        em.createQuery("delete from CiudadEntity").executeUpdate();
+    }
+
+    private List<CiudadEntity> data = new ArrayList<>();
+
+    private void insertData() {
+        for (int i = 0; i < 3; i++) {
+            CiudadEntity entity = factory.manufacturePojo(CiudadEntity.class);
+            em.persist(entity);
+            data.add(entity);
+        }
+    }
+
     @Test
     public void createCiudadTest() {
         CiudadEntity newEntity = factory.manufacturePojo(CiudadEntity.class);
@@ -57,6 +95,51 @@ public class CiudadPersistenceTest {
         Assert.assertNotNull(result);
         CiudadEntity entity = em.find(CiudadEntity.class, result.getId());
         Assert.assertEquals(newEntity.getName(), entity.getName());
+    }
+
+    @Test
+    public void getCiudadesTest() {
+        List<CiudadEntity> list = ciudadPersistence.findAll();
+        Assert.assertEquals(data.size(), list.size());
+        for (CiudadEntity ent : list) {
+            boolean found = false;
+            for (CiudadEntity entity : data) {
+                if (ent.getId().equals(entity.getId())) {
+                    found = true;
+                }
+            }
+            Assert.assertTrue(found);
+        }
+    }
+
+    @Test
+    public void getCiudadTest() {
+        CiudadEntity entity = data.get(0);
+        CiudadEntity newEntity = ciudadPersistence.find(entity.getId());
+        Assert.assertNotNull(newEntity);
+        Assert.assertEquals(entity.getName(), newEntity.getName());
+    }
+
+    @Test
+    public void updateCiudadTest() {
+        CiudadEntity entity = data.get(0);
+        CiudadEntity newEntity = factory.manufacturePojo(CiudadEntity.class);
+
+        newEntity.setId(entity.getId());
+
+        ciudadPersistence.update(newEntity);
+
+        CiudadEntity resp = em.find(CiudadEntity.class, entity.getId());
+
+        Assert.assertEquals(newEntity.getName(), resp.getName());
+    }
+
+    @Test
+    public void deleteCiudadTest() {
+        CiudadEntity entity = data.get(0);
+        ciudadPersistence.delete(entity.getId());
+        CiudadEntity deleted = em.find(CiudadEntity.class, entity.getId());
+        Assert.assertNull(deleted);
     }
 
 }
