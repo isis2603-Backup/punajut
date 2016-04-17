b6f4d37b6d5fcad39ee66d88ec370bcb0c036744
rcardenas11 <r.cardenas11@uniandes.edu.co>
2016-04-17 07:21:04 -0500

    @Stateless en EventoPersistence

--- punajut-logic/src/main/java/co/edu/uniandes/punajut/persistence/EventoPersistence.java
+++ punajut-logic/src/main/java/co/edu/uniandes/punajut/persistence/EventoPersistence.java
@@ -9,6 +9,7 @@ import co.edu.uniandes.punajut.entities.EventoEntity;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.ejb.Stateless;
 import javax.persistence.Query;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -17,6 +18,7 @@ import javax.persistence.PersistenceContext;
  *
  * @author r.cardenas11
  */
+@Stateless
 public class EventoPersistence
 {
    private static final Logger logger = Logger.getLogger(EventoPersistence.class.getName());
