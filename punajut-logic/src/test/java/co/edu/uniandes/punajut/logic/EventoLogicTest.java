577ce979e9d45b26afd9638303fc5f8523e1bbd1
ja.poveda10 <ja.poveda10@172.24.100.225>
2016-04-20 10:50:33 -0500

    Se agregó EventoLogicTest

---
+++ punajut-logic/src/test/java/co/edu/uniandes/punajut/logic/EventoLogicTest.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.punajut.logic;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author ja.poveda10
+ */
+public class EventoLogicTest {
+
+    public EventoLogicTest() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+    }
+
+}
